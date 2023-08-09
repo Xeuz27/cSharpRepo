3487c3e9eac5daca79026d680b4cdc0a35f1e094
Jesus Gutierrez <jesusg.1301.jg@gmail.com>
2023-08-09 16:11:32 -0400

    simple operations with data types

--- helloWorld/helloWorld/Program.cs
+++ helloWorld/helloWorld/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+// programa que calcula el area de un rectangulo
+
+double ladoA;
+double ladoB;
+double resultado;
+
+Console.WriteLine("calcula el area de un rectangulo!");
+Console.WriteLine("ingresa el valor de el lado a");
+ladoA = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("ingresa el valor del lado b");
+ladoB = Convert.ToDouble(Console.ReadLine());
+resultado = ladoB * ladoA;
+Console.WriteLine("el lado a es: "+ladoA+", el lado b es: "+ladoB+", y el resultado es "+resultado);
