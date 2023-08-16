d138b43640a6ec3d0114591b762f60cc55a841c7
Jesus Gutierrez <jesusg.1301.jg@gmail.com>
2023-08-16 15:24:36 -0400

    created project for conditionals lessons

---
+++ conditionals/conditionals/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int playerTotalCount = 0;
+int dealerTotalCount = 0;
+string message = "";
+//black jack minigame
+if (playerTotalCount > dealerTotalCount)
+{
+    message = "you won, congratulations!";
+}else if (playerTotalCount > 21)
+{
+    message = "you lost, try again";
+}
+else if (playerTotalCount <= dealerTotalCount)
+{
+    message = "you lost, try again";
+}
+else
+{
+    message = "invalid condition";
+}
