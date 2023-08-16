8c82008e46b93ee5158c0fff613d1e850cd00933
Jesus Gutierrez <jesusg.1301.jg@gmail.com>
2023-08-16 15:10:28 -0400

    created project for strings lessons

---
+++ strings/strings/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ricardo perez ID");
+int altura = 168;
+int edad = 89;
+string nombre = "Ricardo Perez";
+string informacion = "nacio en hidalgo, es estudiante de gastronomia y astronomia";
+var hobby = "deportista";
+
+string tarjetaDeIdentificacion = $"la informacion de {nombre} es: \n " +
+    $"{informacion} y es {hobby} y tiene {edad} años y mide {altura}";
+
+Console.WriteLine(tarjetaDeIdentificacion);
